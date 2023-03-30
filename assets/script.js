b12883fb7ac898bc416982e0e2e5ef3983b1f3e5
obelisk477 <cwbatte2@gmail.com>
2023-03-29 22:55:19 -0400

    Created main generatePassword function

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,49 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+
+// Function to take data in from prompts and create bassword based on input
+function generatePassword() {
+
+  // Define strings that contain all possible characters in their respective sets
+  var specials =  "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+  var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowers = uppers.toLowerCase()
+  var numerics = "0123456789"
+  var password = ""
+  
+  // Get inputs for password length and parameters from user input
+  var charLen = getCharLen()
+  var hasLowerCase = getBool("Do you want your password to contain lowercase characters? (y/n)")
+  var hasUpperCase = getBool("Do you want your password to contain uppercase characters? (y/n)")
+  var hasNumeric = getBool("Do you want your password to contain numeric characters? (y/n)")
+  var hasSpecial = getBool("Do you want your password to contain special characters? (y/n)")
+
+  // Checks that at least one character type was selected
+  if (!(hasLowerCase || hasUpperCase || hasNumeric || hasSpecial)) {
+    alert("You did not select any character types to include in your password. Please try again.")
+    return
+  }
+  
+  // Prepares character groups for concatenation by setting them to blank if user doesn't want
+  var specials = hasSpecial ? specials : ""
+  var uppers = hasUpperCase ? uppers : ""
+  var numerics = hasNumeric ? numerics : ""
+  var lowers = hasLowerCase ? lowers : ""
+  
+  // Creates string of all preferred password characters
+  var allowedChars = specials + uppers + numerics + lowers
+
+  // Randomly selects a character from above string as many times as the user specifies, and concats to password
+  for (var i=0; i < charLen ; i++) {
+    password += allowedChars[Math.floor(Math.random()*allowedChars.length)]
+  }
+
+  return password
+}
+
+
+
 // Helper function for generatePassword() that checks for a numeric value in the prompt between 8 and 128 (inclusive)
 function getCharLen() {
   var len = prompt("Enter your desired character count")
